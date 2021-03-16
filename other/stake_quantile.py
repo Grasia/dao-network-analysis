fd6a6d38781ce013c1f2ca0b3821d2c10ca50aec
FRYoussef <f.r.youssef@hotmail.com>
2021-03-16 14:43:41 +0100

    Added some stat scripts

---
+++ other/stake_quantile.py
@@ -0,0 +1,54 @@
+"""
+   Descp: Prints stakes quantile
+
+   Created on: 16-mar-2021
+
+   Copyright 2021-2022 Youssef 'FRYoussef' El Faqir El Rhazoui
+        <f.r.youssef@hotmail.com>
+"""
+
+import pandas as pd
+import os
+import sys
+
+if __name__ == '__main__':
+    # Target DAOs --> dxDAO, dOrg, Genesis Alpha
+    if len(sys.argv) != 2:
+        print('ERROR: python stake_quantile.py dao_name')
+        exit(1)
+
+    daos: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'daos.csv'), header=0)
+    daos = daos[daos['name'] == sys.argv[1]]
+    if len(daos) == 0:
+        print('ERROR: DAO name not found.')
+        exit(1)
+    
+    dao_id: str = daos['id'].tolist()[0]
+
+    # stakers
+    stakers: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'stakes.csv'), header=0)
+    stakers = stakers[stakers['dao'] == dao_id]
+    stakers.reset_index(inplace=True)
+    stakers = stakers.groupby(['staker']).size().reset_index(name='stakes')
+
+    # all users
+    users: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'reputation_holders.csv'), header=0)
+    users = users[users['dao'] == dao_id]
+    users.reset_index(inplace=True)
+
+    # calculate users who didnt stake
+    users = set(users['address'].tolist())
+    all_stakers = set(stakers['staker'].tolist())
+    non_stake_users = users.difference(all_stakers)
+
+    # add non-vote-users to df
+    for u in non_stake_users:
+        stakers = stakers.append({'staker': u, 'stakes': 0}, ignore_index=True)
+
+    # remove non member stakers
+    for i, row in stakers.iterrows():
+        if row['staker'] not in users:
+            stakers = stakers.drop(i)
+
+    print(f'\nTotal stakes = {sum(stakers["stakes"].tolist())}\n')
+    print(f'{stakers.quantile([.25, .5, .75, .90, .95, .99])}')
---
+++ other/votes_quantile.py
@@ -0,0 +1,49 @@
+"""
+   Descp: Prints votes quantile
+
+   Created on: 16-mar-2021
+
+   Copyright 2021-2022 Youssef 'FRYoussef' El Faqir El Rhazoui
+        <f.r.youssef@hotmail.com>
+"""
+
+import pandas as pd
+import os
+import sys
+
+if __name__ == '__main__':
+    # Target DAOs --> dxDAO, dOrg, Genesis Alpha
+    if len(sys.argv) != 2:
+        print('ERROR: python vote_quantile.py dao_name')
+        exit(1)
+
+    daos: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'daos.csv'), header=0)
+    daos = daos[daos['name'] == sys.argv[1]]
+    if len(daos) == 0:
+        print('ERROR: DAO name not found.')
+        exit(1)
+    
+    dao_id: str = daos['id'].tolist()[0]
+
+    # users who vote
+    voters: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'votes.csv'), header=0)
+    voters = voters[voters['dao'] == dao_id]
+    voters.reset_index(inplace=True)
+    voters = voters.groupby(['voter']).size().reset_index(name='votes')
+
+    # all users
+    users: pd.DataFrame = pd.read_csv(os.path.join('data', 'raw', 'reputation_holders.csv'), header=0)
+    users = users[users['dao'] == dao_id]
+    users.reset_index(inplace=True)
+
+    # calculate users who didnt vote
+    users = set(users['address'].tolist())
+    vote_users = set(voters['voter'].tolist())
+    non_vote_users = users.difference(vote_users)
+
+    # add non-vote-users to df
+    for u in non_vote_users:
+        voters = voters.append({'voter': u, 'votes': 0}, ignore_index=True)
+
+    print(f'\nTotal votes = {sum(voters["votes"].tolist())}\n')
+    print(f'{voters.quantile([.25, .5, .75, .90, .95, .99])}')
